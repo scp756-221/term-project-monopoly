5a81f4a8acbb277d098a8da73514adc7ec8f1cf0
Jingyan Sun <jsa381@sfu.ca>
2022-03-15 18:59:27 -0700

    Add create_playlist()

--- playlist/app.py
+++ playlist/app.py
@@ -22,7 +22,7 @@ import simplejson as json
 app = Flask(__name__)
 
 metrics = PrometheusMetrics(app)
-metrics.info('app_info', 'User process')
+metrics.info('app_info', 'Playlist process')
 
 bp = Blueprint('app', __name__)
 
@@ -49,12 +49,66 @@ def readiness():
     return Response("", status=200, mimetype="application/json")
 
 
+@bp.route('/', methods=['GET'])
+def list_all():
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    # list all songs here
+    return {}
+
+
+@bp.route('/', methods=['POST'])
+def create_playlist():
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        song_list = content['song_list'].strip().split(",")
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    url = db['name'] + '/' + db['endpoint'][1]
+    payload = {"objtype": "playlist", "song_list": song_list}
+    
+    for song_id in song_list:
+        get_song = requests.get(
+            db['name'] + '/' + db['endpoint'][0],
+            params={"objtype": "music", "objkey": song_id},
+            headers={'Authorization': "test"}
+        )
+        if get_song.json()['Count'] == 0:
+            return Response(json.dumps({"error": f"song_id {song_id} not find"}),
+                status=401,
+                mimetype='application/json')
+    response = requests.post(
+        url,
+        json=payload,
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
+
+
+@bp.route('/<playlist_id>', methods=['DELETE'])
+def delete_playlist(playlist_id):
+    pass
+
+
+@bp.route('/<playlist_id>', methods=['GET'])
+def get_playlist(playlist_id):
+    pass
+
+
 app.register_blueprint(bp, url_prefix='/api/v1/playlist/')
 
 # All database calls will have this prefix.  Prometheus metric
 # calls will not---they will have route '/metrics'.  This is
 # the conventional organization.
-app.register_blueprint(bp, url_prefix='/api/v1/user/')
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
